68fcbee97b97f6e3935f1f3c569939ff723bb354
yutakaki <takaki.yu.dev@gmail.com>
2021-11-27 06:24:40 +0900

    finish toggle postMemeForm

--- client/src/components/home/addMeme.jsx
+++ client/src/components/home/addMeme.jsx
@@ -1,63 +1,89 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addMemeAction } from '../../slice/actions/memeAction';
-import { restartMemeError } from '../../slice/memeSlice';
+import { addMemeAction } from "../../slice/actions/memeAction";
+import { restartMemeError } from "../../slice/memeSlice";
 
-const AddMeme = () => {
-  const [preview, setPreview] = useState('');
-  const [status, setStatus] = useState('');
-  const dispatch = useDispatch()
-  const pending = useSelector(state => state.memes.pending);
-  const error = useSelector(state => state.memes.error);
+const AddMeme = ({ profileAndAddMemeRef }) => {
+  const [preview, setPreview] = useState("");
+  const [status, setStatus] = useState("");
+  const dispatch = useDispatch();
+  const pending = useSelector((state) => state.memes.pending);
+  const error = useSelector((state) => state.memes.error);
 
   const handleFileInputChange = (e) => {
     const file = e.target.files[0];
-    if(file){
+    if (file) {
       const reader = new FileReader();
       reader.readAsDataURL(file);
       reader.onload = () => {
         setPreview(reader.result);
       };
     }
-  }
+  };
   const auto_grow = (e) => {
     setStatus(e.target.value);
     e.target.style.height = "20px";
-    e.target.style.height = 25+ e.target.scrollHeight + "px";
-  }
+    e.target.style.height = 25 + e.target.scrollHeight + "px";
+  };
 
-  const postMeme = async(e) => {
+  const postMeme = async (e) => {
     e.preventDefault();
     try {
-      if(preview){
-        const action = await dispatch(addMemeAction({media: preview, status}));
-        if(!action.payload.error) {
-          setStatus('');
-          setPreview('');
-          dispatch(restartMemeError())
+      if (preview) {
+        const action = await dispatch(
+          addMemeAction({ media: preview, status })
+        );
+        if (!action.payload.error) {
+          setStatus("");
+          setPreview("");
+          dispatch(restartMemeError());
+          profileAndAddMemeRef.current.classList.remove("showForm");
         }
       }
     } catch (error) {
       console.log(error);
     }
-  }
+  };
   return (
     <section className="addForm">
       <form onSubmit={postMeme}>
         <p className="error">{error}</p>
-        <textarea value={status} onChange={auto_grow} placeholder="What's your meme"></textarea>
-        <input value={preview} type="text" placeholder="img/gif link" onChange={(e) => setPreview(e.target.value)}/>
-        <div className='addMemeBtns'>
-          <input type="file" id="memeFile" onChange={handleFileInputChange} accept="image/png, image/gif, image/jpeg"/>
-          <label htmlFor="memeFile" >
+        <textarea
+          value={status}
+          onChange={auto_grow}
+          placeholder="What's your meme"></textarea>
+        <input
+          value={preview}
+          type="text"
+          placeholder="img/gif link"
+          onChange={(e) => setPreview(e.target.value)}
+        />
+        <div className="addMemeBtns">
+          <input
+            type="file"
+            id="memeFile"
+            onChange={handleFileInputChange}
+            accept="image/png, image/gif, image/jpeg"
+          />
+          <label htmlFor="memeFile">
             <i className="fa fa-image"></i>
           </label>
-          <input type="submit" disabled={(preview ? false : true) || pending} value={pending ? "uploading..." : !preview ? "submit (must have an img/gif)" : "submit"}/>
+          <input
+            type="submit"
+            disabled={(preview ? false : true) || pending}
+            value={
+              pending
+                ? "uploading..."
+                : !preview
+                ? "submit (must have an img/gif)"
+                : "submit"
+            }
+          />
         </div>
-        { preview && <img src={preview} alt="preview meme"/> }
-      </form> 
+        {preview && <img src={preview} alt="preview meme" />}
+      </form>
     </section>
-  )
-}
+  );
+};
 
-export default AddMeme
+export default AddMeme;
--- client/src/pages/authenticatedPage/home.jsx
+++ client/src/pages/authenticatedPage/home.jsx
@@ -1,47 +1,49 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import AddMeme from "../../components/home/addMeme";
-import Memes from '../../components/home/memes';
-import { viewAllMemeAction } from '../../slice/actions/memeAction';
-import { resetMemeAction } from '../../slice/memeSlice';
+import Memes from "../../components/home/memes";
+import { viewAllMemeAction } from "../../slice/actions/memeAction";
+import { resetMemeAction } from "../../slice/memeSlice";
 import "../../styles/home/home.scss";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const auth = useSelector(state => state.auth.auth);
-  const memes = useSelector(state => state.memes.memes);
+  const auth = useSelector((state) => state.auth.auth);
+  const memes = useSelector((state) => state.memes.memes);
+  const profileAndAddMemeRef = useRef();
 
   useEffect(() => {
-    if(auth){
+    if (auth) {
       dispatch(viewAllMemeAction());
     }
   }, [auth]);
 
   useEffect(() => {
     return () => {
-      dispatch(resetMemeAction())
-    }
+      dispatch(resetMemeAction());
+    };
   }, []);
+
+  const toggleForm = () => {
+    profileAndAddMemeRef.current.classList.toggle("showForm");
+  };
   return (
     <main className="home">
+      <i className="fa fa-plus-circle" onClick={toggleForm}></i>
       <section className="memeFeed">
-        { memes.map(meme => (
-          <Memes meme={meme} key={meme.id}/>
-        )) }
-        
-
+        {memes.map((meme) => (
+          <Memes meme={meme} key={meme.id} />
+        ))}
       </section>
-      <section className="profileAndAddMeme">
+      <section className="profileAndAddMeme" ref={profileAndAddMemeRef}>
         <div className="profileAndAddMeme2">
           <div className="profileAndAddMemeContainer">
-            <AddMeme />
+            <AddMeme profileAndAddMemeRef={profileAndAddMemeRef} />
           </div>
         </div>
-
       </section>
-      
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
